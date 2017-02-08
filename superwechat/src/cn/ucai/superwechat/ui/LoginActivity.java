620edad6cf2d60e1746b37464898cf46afb11f3b
tangchuanzhong <1319902136@qq.com>
2017-02-08 21:01:25 +0800

    实现用户登陆逻辑,添加用户登陆自己服务器相关方法

--- superwechat/src/cn/ucai/superwechat/ui/LoginActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/LoginActivity.java
@@ -30,16 +30,24 @@ import android.widget.Toast;
 
 import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.domain.User;
 import com.hyphenate.easeui.utils.EaseCommonUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatApplication;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.db.SuperWeChatDBManager;
+import cn.ucai.superwechat.domain.Result;
+import cn.ucai.superwechat.net.NetDao;
+import cn.ucai.superwechat.net.OnCompleteListener;
+import cn.ucai.superwechat.utils.CommonUtils;
+import cn.ucai.superwechat.utils.L;
 import cn.ucai.superwechat.utils.MFGT;
+import cn.ucai.superwechat.utils.ResultUtils;
 
 /**
  * Login screen
@@ -59,6 +67,9 @@ public class LoginActivity extends BaseActivity {
 
     private boolean progressShow;
     private boolean autoLogin = false;
+    ProgressDialog pd;
+    String currentUsername;
+    String currentPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,8 +119,8 @@ public class LoginActivity extends BaseActivity {
             Toast.makeText(this, R.string.network_isnot_available, Toast.LENGTH_SHORT).show();
             return;
         }
-        String currentUsername = etUsername.getText().toString().trim();
-        String currentPassword = etPassword.getText().toString().trim();
+        currentUsername = etUsername.getText().toString().trim();
+        currentPassword = etPassword.getText().toString().trim();
 
         if (TextUtils.isEmpty(currentUsername)) {
             Toast.makeText(this, R.string.User_name_cannot_be_empty, Toast.LENGTH_SHORT).show();
@@ -121,7 +132,7 @@ public class LoginActivity extends BaseActivity {
         }
 
         progressShow = true;
-        final ProgressDialog pd = new ProgressDialog(LoginActivity.this);
+        pd= new ProgressDialog(LoginActivity.this);
         pd.setCanceledOnTouchOutside(false);
         pd.setOnCancelListener(new OnCancelListener() {
 
@@ -142,6 +153,45 @@ public class LoginActivity extends BaseActivity {
         SuperWeChatHelper.getInstance().setCurrentUserName(currentUsername);
 
         final long start = System.currentTimeMillis();
+        loginAppServer();
+        loginEMServer();
+    }
+
+    private void loginAppServer() {
+        NetDao.login(this, currentUsername, currentPassword, new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                L.e(TAG,"login,s="+s);
+                if (s!=null){
+                    Result result= ResultUtils.getResultFromJson(s,User.class);
+                    if (result!=null){
+                        loginEMServer();
+                    }else {
+                        pd.dismiss();
+                        if (result.getRetCode()== I.MSG_LOGIN_UNKNOW_USER){
+                            CommonUtils.showShortToast("账号不存在");
+                        }else if (result.getRetCode()==I.MSG_LOGIN_ERROR_PASSWORD){
+                            CommonUtils.showShortToast("账户密码错误");
+                        }else {
+                            CommonUtils.showShortToast(R.string.Login_failed);
+                        }
+                    }
+                }else {
+                    pd.dismiss();
+                    CommonUtils.showShortToast(R.string.Login_failed);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showShortToast(R.string.Login_failed);
+                L.e(TAG,"error="+error);
+            }
+        });
+    }
+
+    private void loginEMServer() {
         // call login method
         Log.d(TAG, "EMClient.getInstance().login");
         EMClient.getInstance().login(currentUsername, currentPassword, new EMCallBack() {

77fad8777498a06770b138c5864f2a8f045dfcaa
tangchuanzhong <1319902136@qq.com>
2017-02-10 16:12:18 +0800

    解决修改推出登陆之后还可以进入MainActivity的bug

--- superwechat/src/cn/ucai/superwechat/ui/SettingsActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/SettingsActivity.java
@@ -439,7 +439,7 @@ public class SettingsActivity extends BaseActivity implements OnClickListener {
                         pd.dismiss();
                         // show login screen
                         finish();
-                        MFGT.gotoLogin(SettingsActivity.this);
+                        MFGT.gotoLoginCleanTask(SettingsActivity.this);
                     }
                 });
             }

c027b47a5158c08f0b4f8ca59500906e7562ec58
tangchuanzhong <1319902136@qq.com>
2017-02-15 16:35:21 +0800

    聊天界面返回消息列表的实现以及隐藏回话标题栏

--- superwechat/src/cn/ucai/superwechat/ui/MainActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/MainActivity.java
@@ -59,6 +59,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.adapter.MainTabAdpter;
@@ -520,6 +521,8 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
         sdkHelper.pushActivity(this);
 
         EMClient.getInstance().chatManager().addMessageListener(messageListener);
+
+
     }
 
     @Override
@@ -619,6 +622,11 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         showExceptionDialogFromIntent(intent);
+
+        boolean isChat=intent.getBooleanExtra(I.BACK_MAIN_FROM_CHAT,false);
+        if (isChat){
+            layoutTabhost.setChecked(0);
+        }
     }
 
     /**

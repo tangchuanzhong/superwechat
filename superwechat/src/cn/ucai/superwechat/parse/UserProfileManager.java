6f18b96f379bb346ab08890918a459121af44aaf
tangchuanzhong <1319902136@qq.com>
2017-02-16 09:35:29 +0800

    修复昨天要重写运行才可以出现联系人bug

--- superwechat/src/cn/ucai/superwechat/parse/UserProfileManager.java
+++ superwechat/src/cn/ucai/superwechat/parse/UserProfileManager.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.hyphenate.EMValueCallBack;
 import com.hyphenate.chat.EMClient;
+
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.SuperWeChatHelper.DataSyncListener;
 import cn.ucai.superwechat.db.DbOpenHelper;
@@ -25,194 +26,199 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserProfileManager {
-	private static final String TAG = UserProfileManager.class.getSimpleName();
+    private static final String TAG = UserProfileManager.class.getSimpleName();
 
-	/**
-	 * application context
-	 */
-	protected Context appContext = null;
+    /**
+     * application context
+     */
+    protected Context appContext = null;
 
-	/**
-	 * init flag: test if the sdk has been inited before, we don't need to init
-	 * again
-	 */
-	private boolean sdkInited = false;
+    /**
+     * init flag: test if the sdk has been inited before, we don't need to init
+     * again
+     */
+    private boolean sdkInited = false;
 
-	/**
-	 * HuanXin sync contact nick and avatar listener
-	 */
-	private List<DataSyncListener> syncContactInfosListeners;
+    /**
+     * HuanXin sync contact nick and avatar listener
+     */
+    private List<DataSyncListener> syncContactInfosListeners;
 
-	private boolean isSyncingContactInfosWithServer = false;
+    private boolean isSyncingContactInfosWithServer = false;
 
-	private EaseUser currentUser;
+    private EaseUser currentUser;
 
-	public UserProfileManager() {
-	}
+    public UserProfileManager() {
+    }
 
-	public synchronized boolean init(Context context) {
-		if (sdkInited) {
-			return true;
-		}
-		ParseManager.getInstance().onInit(context);
-		syncContactInfosListeners = new ArrayList<DataSyncListener>();
-		sdkInited = true;
-		return true;
-	}
+    public synchronized boolean init(Context context) {
+        if (sdkInited) {
+            return true;
+        }
+        ParseManager.getInstance().onInit(context);
+        syncContactInfosListeners = new ArrayList<DataSyncListener>();
+        sdkInited = true;
+        return true;
+    }
 
-	public void addSyncContactInfoListener(DataSyncListener listener) {
-		if (listener == null) {
-			return;
-		}
-		if (!syncContactInfosListeners.contains(listener)) {
-			syncContactInfosListeners.add(listener);
-		}
-	}
+    public void addSyncContactInfoListener(DataSyncListener listener) {
+        if (listener == null) {
+            return;
+        }
+        if (!syncContactInfosListeners.contains(listener)) {
+            syncContactInfosListeners.add(listener);
+        }
+    }
 
-	public void removeSyncContactInfoListener(DataSyncListener listener) {
-		if (listener == null) {
-			return;
-		}
-		if (syncContactInfosListeners.contains(listener)) {
-			syncContactInfosListeners.remove(listener);
-		}
-	}
+    public void removeSyncContactInfoListener(DataSyncListener listener) {
+        if (listener == null) {
+            return;
+        }
+        if (syncContactInfosListeners.contains(listener)) {
+            syncContactInfosListeners.remove(listener);
+        }
+    }
 
-	public void asyncFetchContactInfosFromServer(List<String> usernames, final EMValueCallBack<List<EaseUser>> callback) {
-		if (isSyncingContactInfosWithServer) {
-			return;
-		}
-		isSyncingContactInfosWithServer = true;
-		ParseManager.getInstance().getContactInfos(usernames, new EMValueCallBack<List<EaseUser>>() {
+    public void asyncFetchContactInfosFromServer(List<String> usernames, final EMValueCallBack<List<EaseUser>> callback) {
+        if (isSyncingContactInfosWithServer) {
+            return;
+        }
+        isSyncingContactInfosWithServer = true;
+        ParseManager.getInstance().getContactInfos(usernames, new EMValueCallBack<List<EaseUser>>() {
 
-			@Override
-			public void onSuccess(List<EaseUser> value) {
-				isSyncingContactInfosWithServer = false;
-				// in case that logout already before server returns,we should
-				// return immediately
-				if (!SuperWeChatHelper.getInstance().isLoggedIn()) {
-					return;
-				}
-				if (callback != null) {
-					callback.onSuccess(value);
-				}
-			}
+            @Override
+            public void onSuccess(List<EaseUser> value) {
+                isSyncingContactInfosWithServer = false;
+                // in case that logout already before server returns,we should
+                // return immediately
+                if (!SuperWeChatHelper.getInstance().isLoggedIn()) {
+                    return;
+                }
+                if (callback != null) {
+                    callback.onSuccess(value);
+                }
+            }
 
-			@Override
-			public void onError(int error, String errorMsg) {
-				isSyncingContactInfosWithServer = false;
-				if (callback != null) {
-					callback.onError(error, errorMsg);
-				}
-			}
+            @Override
+            public void onError(int error, String errorMsg) {
+                isSyncingContactInfosWithServer = false;
+                if (callback != null) {
+                    callback.onError(error, errorMsg);
+                }
+            }
 
-		});
+        });
 
-	}
+    }
 
-	public void notifyContactInfosSyncListener(boolean success) {
-		for (DataSyncListener listener : syncContactInfosListeners) {
-			listener.onSyncComplete(success);
-		}
-	}
+    public void notifyContactInfosSyncListener(boolean success) {
+        for (DataSyncListener listener : syncContactInfosListeners) {
+            listener.onSyncComplete(success);
+        }
+    }
 
-	public boolean isSyncingContactInfoWithServer() {
-		return isSyncingContactInfosWithServer;
-	}
+    public boolean isSyncingContactInfoWithServer() {
+        return isSyncingContactInfosWithServer;
+    }
 
-	public synchronized void reset() {
-		isSyncingContactInfosWithServer = false;
-		currentUser = null;
-		PreferenceManager.getInstance().removeCurrentUserInfo();
-	}
+    public synchronized void reset() {
+        isSyncingContactInfosWithServer = false;
+        currentUser = null;
+        PreferenceManager.getInstance().removeCurrentUserInfo();
+    }
 
-	public synchronized EaseUser getCurrentUserInfo() {
-		if (currentUser == null) {
-			String username = EMClient.getInstance().getCurrentUser();
-			currentUser = new EaseUser(username);
-			String nick = getCurrentUserNick();
-			currentUser.setNick((nick != null) ? nick : username);
-			currentUser.setAvatar(getCurrentUserAvatar());
-		}
-		return currentUser;
-	}
+    public synchronized EaseUser getCurrentUserInfo() {
+        if (currentUser == null) {
+            String username = EMClient.getInstance().getCurrentUser();
+            currentUser = new EaseUser(username);
+            String nick = getCurrentUserNick();
+            currentUser.setNick((nick != null) ? nick : username);
+            currentUser.setAvatar(getCurrentUserAvatar());
+        }
+        return currentUser;
+    }
 
-	public boolean updateCurrentUserNickName(final String nickname) {
-		boolean isSuccess = ParseManager.getInstance().updateParseNickName(nickname);
-		if (isSuccess) {
-			setCurrentUserNick(nickname);
-		}
-		return isSuccess;
-	}
+    public boolean updateCurrentUserNickName(final String nickname) {
+        boolean isSuccess = ParseManager.getInstance().updateParseNickName(nickname);
+        if (isSuccess) {
+            setCurrentUserNick(nickname);
+        }
+        return isSuccess;
+    }
 
-	public String uploadUserAvatar(byte[] data) {
-		String avatarUrl = ParseManager.getInstance().uploadParseAvatar(data);
-		if (avatarUrl != null) {
-			setCurrentUserAvatar(avatarUrl);
-		}
-		return avatarUrl;
-	}
+    public String uploadUserAvatar(byte[] data) {
+        String avatarUrl = ParseManager.getInstance().uploadParseAvatar(data);
+        if (avatarUrl != null) {
+            setCurrentUserAvatar(avatarUrl);
+        }
+        return avatarUrl;
+    }
 
-	public void asyncGetCurrentUserInfo(Activity activity) {
-		ParseManager.getInstance().asyncGetCurrentUserInfo(new EMValueCallBack<EaseUser>() {
+    public void asyncGetCurrentUserInfo(Activity activity) {
+        ParseManager.getInstance().asyncGetCurrentUserInfo(new EMValueCallBack<EaseUser>() {
 
-			@Override
-			public void onSuccess(EaseUser value) {
-			    if(value != null){
-   				setCurrentUserNick(value.getNick());
-  				setCurrentUserAvatar(value.getAvatar());
-			    }
-			}
+            @Override
+            public void onSuccess(EaseUser value) {
+                if (value != null) {
+//   				setCurrentUserNick(value.getNick());
+//  				setCurrentUserAvatar(value.getAvatar());
+                }
+            }
 
-			@Override
-			public void onError(int error, String errorMsg) {
+            @Override
+            public void onError(int error, String errorMsg) {
 
-			}
-		});
+            }
+        });
 
-		NetDao.getUserInfoByUsername(activity, EMClient.getInstance().getCurrentUser(), new OnCompleteListener<String>() {
-			@Override
-			public void onSuccess(String s) {
-				if (s != null) {
-					Result result = ResultUtils.getListResultFromJson(s, User.class);
-					if (result != null && result.isRetMsg()) {
-						User user = (User) result.getRetData();
-						Log.e(TAG,"user="+user);
-						if (user != null) {
-							//sava user info to db
-							SuperWeChatHelper.getInstance().saveAppContact(user);
-							setCurrentUserNick(user.getMUserNick());
-							setCurrentUserAvatar(user.getAvatar());
-						}
-					}
-				}
-			}
-			@Override
-			public void onError(String error) {
+        NetDao.getUserInfoByUsername(activity, EMClient.getInstance().getCurrentUser(), new OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                if (s != null) {
+                    Log.e(TAG, "s=" + s);
+                    Result result = ResultUtils.getResultFromJson(s, User.class);
+                    Log.e(TAG, "result=" + result);
+                    if (result != null && result.isRetMsg()) {
+                        User user = (User) result.getRetData();
+                        Log.e(TAG, "user=" + user);
+                        if (user != null) {
+                            //sava user info to db
+                            SuperWeChatHelper.getInstance().saveAppContact(user);
+                            setCurrentUserNick(user.getMUserNick());
+                            setCurrentUserAvatar(user.getAvatar());
+                        }
+                    }
+                }
+            }
 
-			}
-		});
+            @Override
+            public void onError(String error) {
+                Log.e(TAG, "error=" + error);
+            }
+        });
 
-	}
-	public void asyncGetUserInfo(final String username,final EMValueCallBack<EaseUser> callback){
-		ParseManager.getInstance().asyncGetUserInfo(username, callback);
-	}
-	private void setCurrentUserNick(String nickname) {
-		getCurrentUserInfo().setNick(nickname);
-		PreferenceManager.getInstance().setCurrentUserNick(nickname);
-	}
+    }
 
-	private void setCurrentUserAvatar(String avatar) {
-		getCurrentUserInfo().setAvatar(avatar);
-		PreferenceManager.getInstance().setCurrentUserAvatar(avatar);
-	}
+    public void asyncGetUserInfo(final String username, final EMValueCallBack<EaseUser> callback) {
+        ParseManager.getInstance().asyncGetUserInfo(username, callback);
+    }
 
-	private String getCurrentUserNick() {
-		return PreferenceManager.getInstance().getCurrentUserNick();
-	}
+    private void setCurrentUserNick(String nickname) {
+        getCurrentUserInfo().setNick(nickname);
+        PreferenceManager.getInstance().setCurrentUserNick(nickname);
+    }
 
-	private String getCurrentUserAvatar() {
-		return PreferenceManager.getInstance().getCurrentUserAvatar();
-	}
+    private void setCurrentUserAvatar(String avatar) {
+        getCurrentUserInfo().setAvatar(avatar);
+        PreferenceManager.getInstance().setCurrentUserAvatar(avatar);
+    }
+
+    private String getCurrentUserNick() {
+        return PreferenceManager.getInstance().getCurrentUserNick();
+    }
+
+    private String getCurrentUserAvatar() {
+        return PreferenceManager.getInstance().getCurrentUserAvatar();
+    }
 
 }

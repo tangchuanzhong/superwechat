dcf99027f7cab195caab8222d967e4f2d9eb3439
tangchuanzhong <1319902136@qq.com>
2017-02-17 12:03:40 +0800

    添加实现头像功能

--- superwechat/src/cn/ucai/superwechat/net/NetDao.java
+++ superwechat/src/cn/ucai/superwechat/net/NetDao.java
@@ -113,9 +113,9 @@ public class NetDao {
                 .addParam(I.Group.OWNER,group.getOwner())
                 .addParam(I.Group.IS_PUBLIC,String.valueOf(group.isPublic()))
                 .addParam(I.Group.ALLOW_INVITES,String.valueOf(group.isAllowInvites()))
-                .targetClass(String.class)
-               // .addFile2(file)
+                .addFile2(file)
                 .post()
+                .targetClass(String.class)
                 .execute(listener);
     }
 }
